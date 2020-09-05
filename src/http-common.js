8dbaa5fa37221d5cceb526397e51ad2bed15a507
Isaac Araujo <isaac_maciel@hotmail.com>
2020-09-04 23:17:40 -0300

    alteracao no baseUrl

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-api-zhk.herokuapp.com:8708/',
+  baseURL: 'https://grades-api-zhk.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
